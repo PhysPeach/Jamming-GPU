b3f3e8ff328b3450573df433c7dc04220a4be174
PhysPeach <Masato.Shukawa@outlook.jp>
2020-10-31 15:54:59 +0900

    findJamming

--- main/findJamming.cu
+++ main/findJamming.cu
@@ -1,7 +1,11 @@
+#include <cuda.h>
+
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 #include <sstream>
+
+#include <time.h>
 
 #include "../cuh/conf.cuh"
 #include "../cuh/jamming.cuh"
@@ -11,7 +15,9 @@ int Np;
 double Phi_init;
 double Dphi;
 int main(int argc, char** argv) {
+
     ID = atoi(argv[1]);
+    init_genrand((unsigned long)time(NULL)*ID + ID);
     Np = atoi(argv[2]);
     Phi_init = atof(argv[3]);
     Dphi = 0.;
@@ -23,13 +29,19 @@ int main(int argc, char** argv) {
     std::cout << "------------------" << std::endl << std::endl;
 
     PhysPeach::Jamming jam;
-    PhysPeach::loadSwapMC(&jam);
+    if(Phi_init * Phi_init > 0.0001){
+        PhysPeach::loadSwapMC(&jam);
+    }else{
+        Phi_init = 0.82;
+        PhysPeach::createJamming(&jam);
+        Phi_init = 0.;
+    }
     PhysPeach::fireJamming(&jam);
 
     double phimem;
-    double *xmem;
-    xmem = (double*)malloc(D*Np*sizeof(double));
-    memcpy(xmem, jam.p.x, D*Np*sizeof(double));
+    double *xmem_dev;
+    cudaMalloc((void**)&xmem_dev, D * Np * sizeof(double));
+    cudaMemcpy(xmem_dev, jam.p.x_dev, D * Np * sizeof(double), cudaMemcpyDeviceToDevice);
 
     double Pnow = P(&jam.p, L(&jam), &jam.lists);
     double loop = 0;
@@ -38,13 +50,13 @@ int main(int argc, char** argv) {
     std::cout << "    phi, E, P, loop:" << std::endl;
     while (jam.phi < 0.84 || Pnow < 1.0e-8){
         phimem = jam.phi;
-        memcpy(xmem, jam.p.x, D*Np*sizeof(double));
+        cudaMemcpy(xmem_dev, jam.p.x_dev, D * Np * sizeof(double), cudaMemcpyDeviceToDevice);
         loop = PhysPeach::addDphi(&jam, 1.0e-4);
         Pnow = P(&jam.p, L(&jam), &jam.lists);
         std::cout << "    " << jam.phi << ", " << U(&jam.p, L(&jam), &jam.lists) << ", " << Pnow << ", " << loop << std::endl;
     }
     jam.phi = phimem;
-    memcpy(jam.p.x, xmem, D*Np*sizeof(double));
+    cudaMemcpy(jam.p.x_dev, xmem_dev, D * Np * sizeof(double), cudaMemcpyDeviceToDevice);
     Pnow = P(&jam.p, L(&jam), &jam.lists);
 
     std::cout << "    Squeeze from phi = " << jam.phi << " by dphi = " << 1.0e-5 << std::endl;
@@ -55,21 +67,22 @@ int main(int argc, char** argv) {
         std::cout << "    " << jam.phi << ", " << U(&jam.p, L(&jam), &jam.lists) << ", " << Pnow << ", " << loop << std::endl;
     }
     jam.phi = phimem;
-    memcpy(xmem, jam.p.x, D*Np*sizeof(double));
+    cudaMemcpy(xmem_dev, jam.p.x_dev, D * Np * sizeof(double), cudaMemcpyDeviceToDevice);
     Pnow = P(&jam.p, L(&jam), &jam.lists);
 
     std::cout << "    Squeeze from phi = " << jam.phi << " by dphi = " << 1.0e-6 << std::endl;
     std::cout << "    phi, E, P, loop:" << std::endl;
     while (Pnow > 1.0e-8){
         phimem = jam.phi;
-        memcpy(xmem, jam.p.x, D*Np*sizeof(double));
+        cudaMemcpy(xmem_dev, jam.p.x_dev, D * Np * sizeof(double), cudaMemcpyDeviceToDevice);
         loop = PhysPeach::addDphi(&jam, -1.0e-6);
         Pnow = P(&jam.p, L(&jam), &jam.lists);
         std::cout << "    " << jam.phi << ", " << U(&jam.p, L(&jam), &jam.lists) << ", " << Pnow << ", " << loop << std::endl;
     }
     jam.phi = phimem;
-    memcpy(jam.p.x, xmem, D*Np*sizeof(double));
+    cudaMemcpy(jam.p.x_dev, xmem_dev, D * Np * sizeof(double), cudaMemcpyDeviceToDevice);
     std::cout << "-> Jamming Point: " << jam.phi << std::endl;
+    cudaFree(xmem_dev);
 
     std::ofstream file;
 
@@ -83,6 +96,7 @@ int main(int argc, char** argv) {
     posName << "../pos/pos_N" << Np << "_Phi" << Phi_init << "_id" << ID <<".data";
     file.open(posName.str().c_str());
     file << std::setprecision(15);
+    cudaMemcpy(jam.p.x, jam.p.x_dev, D * Np * sizeof(double), cudaMemcpyDeviceToHost);
     for(int par1 = 0; par1 < Np; par1++){
         file << jam.p.diam[par1] << " ";
         for(int d = 0; d < D; d++){
